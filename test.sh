2f53cd2736cb2f65f5ff00db19ceaa500e330a0e
Kevin Orr <kevino@kududyn.com>
2024-01-29 13:06:03 -0500

    Test with cabal as well

--- test.sh
+++ test.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env sh
 
+if [ $# -ne 1 ]; then
+    echo "USAGE: $0 {stack | cabal}" >&2
+    exit 2
+fi
+
 set -eux
 
 mkdir -p /tmp/mylib_d
 gcc mylib.c -o /tmp/mylib_d/libmylib.dylib -shared
 install_name_tool -id '@rpath/libmylib.dylib' /tmp/mylib_d/libmylib.dylib
-stack purge
-stack build
+
+case $1 in
+    stack)
+        stack purge
+        stack build
+        ;;
+    cabal)
+        cabal build all
+        ;;
+    *)
+        echo "USAGE: $0 {stack | cabal}" >&2
+        exit 2
+esac
